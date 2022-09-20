255b96b20c62e0c2006ef4f0227ca0b06435201b
NotEternal <cent1pede@protonmail.ch>
2022-09-20 18:00:01 +0200

    style(swaps): remove swap link and improve form notice
    - remove a swap link from the header
    - improve UI of swap form notice

--- packages/frontend/src/pages/TokenSwap/ui/Notification.js
+++ packages/frontend/src/pages/TokenSwap/ui/Notification.js
@@ -2,21 +2,38 @@ import React from 'react';
 import { Translate } from 'react-localize-redux';
 import styled from 'styled-components';
 
+import AlertTriangleIcon from '../../../components/svg/AlertTriangleIcon';
+
 const NotificationWrapper = styled.p`
     margin: 0;
-    padding: 0.6rem;
+    padding: 0.75rem;
+    display: flex;
     font-size: 1.1rem;
-    border-radius: 0.5rem;
+    border-radius: 0.25rem;
 
     &.warning {
-        background-color: #FFDBB2;
+        color: var(--color-warning);
+        background-color: var(--color-warning-background);
     }
+
+    span {
+        margin-left: 0.6rem;
+    }
+`;
+
+const IconWrapper = styled.span`
+    padding: 0.1rem;
 `;
 
 export default function Notification({ id, data, type }) {
     return (
         <NotificationWrapper className={`${type}`}>
-            <Translate id={id} data={data} />
+            <IconWrapper>
+                <AlertTriangleIcon />
+            </IconWrapper>
+            <span>
+                <Translate id={id} data={data} />
+            </span>
         </NotificationWrapper>
     );
 }
