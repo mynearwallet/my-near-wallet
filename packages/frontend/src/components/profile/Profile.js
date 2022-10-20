b5fd7d3e10e651aa96298f9d454848541f1dc386
Lapko <artyom.lapko@ostrovok.ru>
2022-10-20 13:33:48 +0400

    fix: broken path

--- packages/frontend/src/components/profile/Profile.js
+++ packages/frontend/src/components/profile/Profile.js
@@ -42,7 +42,7 @@ import BalanceContainer from './balances/BalanceContainer';
 import LockupAvailTransfer from './balances/LockupAvailTransfer';
 import ExportKeyWrapper from './export_private_key/ExportKeyWrapper';
 import MobileSharingWrapper from './mobile_sharing/MobileSharingWrapper';
-import { Recovery } from './Recovery';
+import { Recovery } from './recovery';
 import RemoveAccountWrapper from './remove_account/RemoveAccountWrapper';
 import TwoFactorAuth from './two_factor/TwoFactorAuth';
 import { StyledContainer } from './ui';
