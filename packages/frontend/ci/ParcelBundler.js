e1a8836c86ff62f695805f06a5c92c36ad68e9a8
Den Ilin <followdarko@gmail.com>
2022-10-27 19:37:04 +0300

    fix: fixed configs

--- packages/frontend/ci/ParcelBundler.js
+++ packages/frontend/ci/ParcelBundler.js
@@ -6,7 +6,8 @@ const Bundler = require('parcel-bundler');
 const Config = require('./config');
 
 const DIST_PATH = path.join(__dirname, '../dist/static');
-const DIST_INDEX_PATH = path.join(__dirname, '../dist');
+const ENTRY_INDEX_PATH = path.join(__dirname, '../dist/static/index.html');
+const DIST_INDEX_PATH = path.join(__dirname, '../dist/index.html');
 const ENTRY_FILE_PATH = path.join(__dirname, '../src/index.html');
 const WASM_PATH = path.join(__dirname, '../src/wasm/');
 const SSL_PATH = path.join(__dirname, '../devServerCertificates/');
@@ -16,7 +17,6 @@ const enableDebugLogging = Config.DEBUG_BUILD;
 class ParcelBundler {
     constructor({
         outDir = DIST_PATH,
-        outIndexDir = DIST_INDEX_PATH,
         entryPath = ENTRY_FILE_PATH,
         wasmSourcePath = WASM_PATH,
         sslPath = SSL_PATH,
@@ -29,7 +29,6 @@ class ParcelBundler {
     } = {}) {
         this.entryPath = entryPath;
         this.outDir = outDir;
-        this.outIndexDir = outIndexDir;
         this.wasmSourcePath = wasmSourcePath;
         this.sslPath = sslPath;
         this.isDebug = isDebug;
@@ -53,7 +52,7 @@ class ParcelBundler {
 
     getBaseConfig() {
         return {
-            outDir: this.outIndexDir,
+            outDir: this.outDir,
             outFile: 'index.html',
             logLevel: 3, // 5 = save everything to a file, 4 = like 3, but with timestamps and additionally log http requests to dev server, 3 = log info, warnings & errors, 2 = log warnings & errors, 1 = log errors, 0 = log nothing
             watch: this.isDevelopment,
@@ -196,6 +195,7 @@ class ParcelBundler {
             fs.copyFileSync(this.buildWasmSourcePath('multisig.wasm'), this.buildOutputPath('multisig.wasm'));
             fs.copyFileSync(this.buildWasmSourcePath('main.wasm'), this.buildOutputPath('main.wasm'));
             fs.copyFileSync(this.buildWasmSourcePath('state_cleanup.wasm'), this.buildOutputPath('state_cleanup.wasm'));
+            fs.copyFileSync(ENTRY_INDEX_PATH, DIST_INDEX_PATH);
         });
 
         return this.bundler;
