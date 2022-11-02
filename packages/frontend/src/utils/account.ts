d75acfd2286f8ef8f5cb610c9ad6183305028807
Lapko <artyom.lapko@ostrovok.ru>
2022-11-02 19:16:04 +0400

    fix: fix

--- packages/frontend/src/utils/account.ts
+++ packages/frontend/src/utils/account.ts
@@ -1,18 +1,18 @@
 import { IMPLICIT_ACCOUNT_MAX_LENGTH } from './constants';
 import { KEY_ACTIVE_ACCOUNT_ID } from './wallet';
 
-export const validateEmail = (email: string): boolean => {
+export const validateEmail = (email) => {
     /* Checks for anystring@anystring.anystring */
     let re = /\S+@\S+\.\S+/;
     return re.test(email);
 };
 
-export const isImplicitAccount = (accountId: string): boolean =>
+export const isImplicitAccount = (accountId) =>
     accountId && accountId.length === IMPLICIT_ACCOUNT_MAX_LENGTH && !accountId.includes('.');
 
 const ACCOUNT_ID_SEPARATOR = '...';
 
-export const shortenAccountId = (id: string, startChars = 8, endChars = 8): string => {
+export const shortenAccountId = (id, startChars = 8, endChars = 8) => {
     const numOfRemainingChars = startChars + endChars + ACCOUNT_ID_SEPARATOR.length;
     const isOutOfScope = id.length < numOfRemainingChars || numOfRemainingChars > IMPLICIT_ACCOUNT_MAX_LENGTH;
     const isInvalidCharCount = startChars < 1 || endChars < 1;
@@ -24,5 +24,5 @@ export const shortenAccountId = (id: string, startChars = 8, endChars = 8): stri
     return `${id.slice(0, startChars)}${ACCOUNT_ID_SEPARATOR}${id.slice(id.length - endChars)}`;
 };
 
-export const getActiveAccountId = (): string|null =>
+export const getActiveAccountId = () =>
     localStorage.getItem(KEY_ACTIVE_ACCOUNT_ID);
