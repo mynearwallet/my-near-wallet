c7e62b5a27f716053877a6b67b3ba4c92b01659a
loosefer <e-basta@list.ru>
2022-11-10 18:04:55 +0400

    feat: deauth by inactivity (#90)
    Co-authored-by: Lapko <artyom.lapko@ostrovok.ru>

--- packages/frontend/src/components/common/routing/PrivateRoute/PrivateRoute.js
+++ packages/frontend/src/components/common/routing/PrivateRoute/PrivateRoute.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import { connect } from 'react-redux';
 import { Route, withRouter, Redirect } from 'react-router-dom';
 
+import { setAuthorizedByPassword } from '../../../../redux/reducers/security';
 import { selectAccountSlice } from '../../../../redux/slices/account';
 import { selectStatusLocalAlert } from '../../../../redux/slices/status';
 import { getActiveAccountId } from '../../../../utils/account';
 import { isEncrypted } from '../../../../utils/encryption/keys';
 import NoIndexMetaTag from '../../NoIndexMetaTag';
 
+const DEAUTH_TIMEOUT = 1000 * 60 * 60;
 
 /**
  * PrivateRoute is for logged in users only and will redirect to the guest landing page
@@ -19,53 +21,79 @@ const PrivateRoute = ({
     account,
     indexBySearchEngines,
     isAuthorizedByPassword,
+    setAuthorized,
     ...rest
-}) => (
-    <>
-        {!indexBySearchEngines && <NoIndexMetaTag />}
-        <Route
-            render={(props) => {
-                if (!getActiveAccountId()) {
-                    return (
-                        <Redirect
-                            to={{
-                                pathname: '/',
-                            }}
-                        />
-                    );
-                }
+}) => {
+    const [inactivityTimer, setInactivityTimer] = useState(null);
 
-                const isBlockedByPassword = isEncrypted() && !isAuthorizedByPassword;
-                if (isBlockedByPassword) {
-                    // Avoid infinite loop of redirect
-                    const skipRedirect = location.pathname === '/enter-password';
+    const handleFocus = useCallback(() => {
+        clearTimeout(inactivityTimer);
+        setInactivityTimer(null);
+    }, [inactivityTimer]);
 
-                    if (!skipRedirect) {
+    const handleBlur = useCallback(() => {
+        setInactivityTimer(setTimeout(() => {
+            setAuthorized(false);
+        }, DEAUTH_TIMEOUT));
+    }, [inactivityTimer]);
+
+    useEffect(() => {
+        window.addEventListener('focus', handleFocus);
+        window.addEventListener('blur', handleBlur);
+
+        return () => {
+            window.removeEventListener('focus', handleFocus);
+            window.removeEventListener('blur', handleBlur);
+        };
+    }, [handleFocus, handleBlur]);
+
+    return (
+        <>
+            {!indexBySearchEngines && <NoIndexMetaTag />}
+            <Route
+                render={(props) => {
+                    if (!getActiveAccountId()) {
                         return (
                             <Redirect
                                 to={{
-                                    pathname: '/enter-password',
-                                    search: encodeURIComponent(location.pathname)
+                                    pathname: '/',
                                 }}
                             />
                         );
                     }
-                }
 
-                // <Route component> takes precedence over <Route render></Route>
-                if (Component) {
-                    return <Component {...props} />;
-                }
+                    const isBlockedByPassword = isEncrypted() && !isAuthorizedByPassword;
+                    if (isBlockedByPassword) {
+                        // Avoid infinite loop of redirect
+                        const skipRedirect = location.pathname === '/enter-password';
 
-                if (render) {
-                    return render(props);
-                }
+                        if (!skipRedirect) {
+                            return (
+                                <Redirect
+                                    to={{
+                                        pathname: '/enter-password',
+                                        search: encodeURIComponent(location.pathname)
+                                    }}
+                                />
+                            );
+                        }
+                    }
 
-                return (<></>);
-            }}
-        />
-    </>
-);
+                    // <Route component> takes precedence over <Route render></Route>
+                    if (Component) {
+                        return <Component {...props} />;
+                    }
+
+                    if (render) {
+                        return render(props);
+                    }
+
+                    return (<></>);
+                }}
+            />
+        </>
+    );
+};
 
 const mapStateToProps = (state) => ({
     isAuthorizedByPassword: state.security.isAuthorizedByPassword,
@@ -73,4 +101,8 @@ const mapStateToProps = (state) => ({
     localAlert: selectStatusLocalAlert(state)
 });
 
-export default withRouter(connect(mapStateToProps)(PrivateRoute));
+const mapDispatchToProps = {
+    setAuthorized: setAuthorizedByPassword
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(PrivateRoute));
