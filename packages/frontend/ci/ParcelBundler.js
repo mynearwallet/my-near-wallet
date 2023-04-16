bd051f7b267a71e1b4fb52e9b05c722773a09133
DominicGBauer <dominic@nomanini.com>
2023-04-16 13:01:17 +0200

    chore: update parcel

--- packages/frontend/ci/ParcelBundler.js
+++ packages/frontend/ci/ParcelBundler.js
@@ -1,22 +1,19 @@
-const fs = require("fs");
 const path = require("path");
-
-const Bundler = require("parcel-bundler");
-
+const ParcelCore = require("@parcel/core");
+const { default: Parcel } = ParcelCore;
 const Config = require("./config");
 
 const DIST_PATH = path.join(__dirname, "../dist");
+const CACHE_PATH = path.join(__dirname, "../.parcel-cache");
 const ENTRY_FILE_PATH = path.join(__dirname, "../src/index.html");
-const WASM_PATH = path.join(__dirname, "../src/wasm/");
 const SSL_PATH = path.join(__dirname, "../devServerCertificates/");
 
 const enableDebugLogging = Config.DEBUG_BUILD;
 
 class ParcelBundler {
   constructor({
-    outDir = DIST_PATH,
-    entryPath = ENTRY_FILE_PATH,
-    wasmSourcePath = WASM_PATH,
+    distDir = DIST_PATH,
+    entries = ENTRY_FILE_PATH,
     sslPath = SSL_PATH,
     cloudflareBaseUrl = Config.CLOUDFLARE_BASE_URL,
     shouldUseCloudflare = Config.SHOULD_USE_CLOUDFLARE,
@@ -25,9 +22,8 @@ class ParcelBundler {
     isNetlify = Config.IS_NETLIFY,
     isDevelopment = Config.IS_DEVELOPMENT,
   } = {}) {
-    this.entryPath = entryPath;
-    this.outDir = outDir;
-    this.wasmSourcePath = wasmSourcePath;
+    this.entries = entries;
+    this.distDir = distDir;
     this.sslPath = sslPath;
     this.isDebug = isDebug;
     this.isRender = isRender;
@@ -49,24 +45,18 @@ class ParcelBundler {
 
   getBaseConfig() {
     return {
-      outDir: this.outDir,
-      outFile: "index.html",
       logLevel: 3, // 5 = save everything to a file, 4 = like 3, but with timestamps and additionally log http requests to dev server, 3 = log info, warnings & errors, 2 = log warnings & errors, 1 = log errors, 0 = log nothing
-      watch: this.isDevelopment,
-      hmr: this.isDevelopment,
-      sourceMaps: true,
+      shouldAutoInstall: true,
+      mode: this.isDevelopment ? 'development' : 'production',
       detailedReport: false,
-      autoInstall: true,
-      minify: !this.isDevelopment,
+      defaultConfig: '@parcel/config-default',
+      cacheDir: CACHE_PATH,
+      defaultTargetOptions: {
+        shouldOptimize: !this.isDevelopment,
+        distDir: this.distDir,
+        sourceMaps: true,
+      }
     };
-  }
-
-  buildOutputPath(filename) {
-    return path.join(this.outDir, filename);
-  }
-
-  buildWasmSourcePath(filename) {
-    return path.join(this.wasmSourcePath, filename);
   }
 
   buildSslPath(filename) {
@@ -98,7 +88,10 @@ class ParcelBundler {
 
       return {
         ...this.getBaseConfig(),
-        publicUrl: this.buildCloudflarePath(`/rnd/pr/${prNumber[1]}/`),
+        defaultTargetOption: {
+            ...this.getBaseConfig().defaultTargetOption,
+            publicUrl: this.buildCloudflarePath(`/rnd/pr/${prNumber[1]}/`),
+        }
       };
     }
 
@@ -128,33 +121,49 @@ class ParcelBundler {
           // Netlify staging is a dedicated deployment using 'master' as the production branch
           return {
             ...this.getBaseConfig(),
-            publicUrl: this.buildCloudflarePath("/ntl/staging/"),
+            defaultTargetOption: {
+                ...this.getBaseConfig().defaultTargetOption,
+                publicUrl: this.buildCloudflarePath("/ntl/staging/"),
+            }
           };
+
         }
 
         // Netlify production/mainnet is a dedicated deployment using 'stable' as the production branch
         return {
           ...this.getBaseConfig(),
-          publicUrl: this.buildCloudflarePath("/ntl/mainnet/"),
+          defaultTargetOption: {
+            ...this.getBaseConfig().defaultTargetOption,
+            publicUrl: this.buildCloudflarePath("/ntl/mainnet/"),
+          }
         };
 
       case "branch-deploy":
         return {
           ...this.getBaseConfig(),
-          publicUrl: this.buildCloudflarePath(`/ntl/branch/${branchName}/`),
+          defaultTargetOption: {
+            ...this.getBaseConfig().defaultTargetOption,
+            publicUrl: this.buildCloudflarePath(`/ntl/branch/${branchName}/`),
+          }
         };
       case "deploy-preview":
         if (primeUrl.includes("near-wallet-staging")) {
           // Netlify staging is a dedicated deployment using 'master' as the production branch
           return {
             ...this.getBaseConfig(),
-            publicUrl: this.buildCloudflarePath(`/ntl/previewstaging/${pullRequestId}/`),
+            defaultTargetOption: {
+                ...this.getBaseConfig().defaultTargetOption,
+                publicUrl: this.buildCloudflarePath(`/ntl/previewstaging/${pullRequestId}/`),
+              }
           };
         }
 
         return {
           ...this.getBaseConfig(),
-          publicUrl: this.buildCloudflarePath(`/ntl/preview/${pullRequestId}/`),
+          defaultTargetOption: {
+            ...this.getBaseConfig().defaultTargetOption,
+            publicUrl: this.buildCloudflarePath(`/ntl/preview/${pullRequestId}/`),
+          }
         };
       default:
         throw new Error("Could not identify Netlify build environment");
@@ -183,20 +192,9 @@ class ParcelBundler {
   initializeBundlerInstance() {
     const bundlerConfig = this.composeBundlerConfig();
 
-    this.debugLog("entryPath", this.entryPath);
+    this.debugLog("entries", this.entries);
     this.debugLog("bundlerConfig", bundlerConfig);
-    this.bundler = new Bundler(this.entryPath, bundlerConfig);
-    this.bundler.on("bundled", (bundle) => {
-      fs.copyFileSync(
-        this.buildWasmSourcePath("multisig.wasm"),
-        this.buildOutputPath("multisig.wasm"),
-      );
-      fs.copyFileSync(this.buildWasmSourcePath("main.wasm"), this.buildOutputPath("main.wasm"));
-      fs.copyFileSync(
-        this.buildWasmSourcePath("state_cleanup.wasm"),
-        this.buildOutputPath("state_cleanup.wasm"),
-      );
-    });
+    this.bundler = new Parcel({...bundlerConfig, entries: this.entries});
 
     return this.bundler;
   }
@@ -212,7 +210,7 @@ class ParcelBundler {
         key: this.buildSslPath("private.pem"),
       });
     } else {
-      await this.bundler.bundle();
+      await this.bundler.run();
     }
   }
 }
