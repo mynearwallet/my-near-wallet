6dcaa2e2b9a1bc430ca5be4bfe5208b2ab5b1620
Dominic Gunther Bauer <46312751+DominicGBauer@users.noreply.github.com>
2023-04-18 10:58:36 +0200

    chore: update parcel and jest (#112)
    * chore: update jest
    
    * chore: update parcel
    
    * fix: ci issues
    
    ---------
    
    Co-authored-by: DominicGBauer <dominic@nomanini.com>

--- packages/frontend/jest.config.js
+++ packages/frontend/jest.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-  setupFilesAfterEnv: ["./jest.setup.js"],
   setupFiles: ["dotenv/config"],
   testEnvironment: "jsdom",
+  preset: 'ts-jest/presets/js-with-ts-esm',
   moduleNameMapper: {
     "^.+\\.svg$": "jest-svg-transformer",
+  },
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': [
+      'ts-jest',
+      {
+        diagnostics: true,
+      },
+    ],
   },
 };
