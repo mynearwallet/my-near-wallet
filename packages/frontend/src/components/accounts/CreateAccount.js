f978322e362dc9f45e8c406c5fea5610cb0ccb5e
Paul Myburgh <lostpebble@protonmail.com>
2023-04-21 18:20:49 +0200

    feat: To reverted master: Add new linkdrop standard fixes & trial account offboarding (#120)
    * feat: squash and rebasing of trial account changes into a clean commit
    
    * fix: add in loading indicator for link drop, fix confusing UI on landing at linkdrop

--- packages/frontend/src/components/accounts/CreateAccount.js
+++ packages/frontend/src/components/accounts/CreateAccount.js
@@ -8,7 +8,7 @@ import styled from 'styled-components';
 import CONFIG from '../../config';
 import { Mixpanel } from '../../mixpanel/index';
 import {
-    checkNearDropBalance,
+    checkLinkdropInfo,
     checkNewAccount,
     redirectTo,
     refreshAccount
@@ -110,8 +110,8 @@ class CreateAccount extends Component {
     state = {
         loader: false,
         accountId: '',
-        invalidNearDrop: null,
-        fundingAmount: null,
+        invalidLinkdrop: null,
+        linkdropKeyInfo: null,
         termsAccepted: false,
         whereToBuy: false
     }
@@ -120,7 +120,7 @@ class CreateAccount extends Component {
         const { fundingContract, fundingKey } = this.props;
 
         if (fundingContract && fundingKey) {
-            this.handleCheckNearDropBalance();
+            this.handleCheckLinkdropInfo();
         }
     }
 
@@ -128,14 +128,14 @@ class CreateAccount extends Component {
         this.props.clearLocalAlert();
     }
 
-    handleCheckNearDropBalance = async () => {
-        const { fundingContract, fundingKey, checkNearDropBalance } = this.props;
+    handleCheckLinkdropInfo = async () => {
+        const { fundingContract, fundingKey, checkLinkdropInfo } = this.props;
         await Mixpanel.withTracking('CA Check near drop balance',
             async () => {
-                const fundingAmount = await checkNearDropBalance(fundingContract, fundingKey);
-                this.setState({ fundingAmount });
+                const linkdropKeyInfo = await checkLinkdropInfo(fundingContract, fundingKey);
+                this.setState({ linkdropKeyInfo });
             },
-            () => this.setState({ invalidNearDrop: true })
+            () => this.setState({ invalidLinkdrop: true })
         );
     }
 
@@ -148,7 +148,7 @@ class CreateAccount extends Component {
     }
 
     handleCreateAccount = async () => {
-        const { accountId, fundingAmount } = this.state;
+        const { accountId, linkdropKeyInfo } = this.state;
         const {
             fundingContract, fundingKey,
             fundingAccountId,
@@ -158,7 +158,7 @@ class CreateAccount extends Component {
 
         let queryString = '';
         if (fundingAccountId || fundingContract) {
-            const fundingOptions = fundingAccountId ? { fundingAccountId } : { fundingContract, fundingKey, fundingAmount };
+            const fundingOptions = fundingAccountId ? { fundingAccountId } : { fundingContract, fundingKey, fundingAmount: linkdropKeyInfo.yoctoNEAR };
             queryString = `?fundingOptions=${encodeURIComponent(JSON.stringify(fundingOptions))}`;
         }
         Mixpanel.track('CA Click create account button');
@@ -166,13 +166,7 @@ class CreateAccount extends Component {
     }
 
     render() {
-        const {
-            loader,
-            accountId,
-            invalidNearDrop,
-            termsAccepted,
-            whereToBuy
-        } = this.state;
+        const { loader, accountId, invalidLinkdrop, termsAccepted, whereToBuy } = this.state;
 
         const {
             localAlert,
@@ -242,7 +236,7 @@ class CreateAccount extends Component {
             );
         }
 
-        if (!invalidNearDrop) {
+        if (!invalidLinkdrop) {
             return (
                 <StyledContainer className='small-centered border'>
                     <form onSubmit={(e) => {
@@ -315,7 +309,7 @@ const mapDispatchToProps = {
     checkNewAccount,
     clearLocalAlert,
     refreshAccount,
-    checkNearDropBalance,
+    checkLinkdropInfo,
     redirectTo
 };
 
