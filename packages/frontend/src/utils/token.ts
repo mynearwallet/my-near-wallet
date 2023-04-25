2226a3496c4de6dfa15d86c88933d8f608e70ae9
Paul Myburgh <lostpebble@protonmail.com>
2023-04-25 17:03:49 +0200

    refactor: use a map pattern to store token meta instead of if / return

--- packages/frontend/src/utils/token.ts
+++ packages/frontend/src/utils/token.ts
@@ -1,42 +1,44 @@
 export const BRIDGED_CONSTANT = 'Bridged';
 
-export const formatToken = (token: Wallet.Token) => {
+interface ISpecialTokenMeta {
+    symbol: string;
+}
+
+const specialTokenMetaMap: { [contractName: string]: ISpecialTokenMeta } = {
+    // USDT Bridged
+    ['usdt.fakes.testnet']: {
+        symbol: `${BRIDGED_CONSTANT} USDT`,
+    },
+    // USDT Native
+    ['usdt.tether-token.near']: {
+        symbol: 'Native USDT',
+    },
+    // USDC Bridged
+    ['usdc.fakes.testnet']: {
+        symbol: `${BRIDGED_CONSTANT} USDC`,
+    },
+};
+
+// USDT Bridged
+specialTokenMetaMap['dac17f958d2ee523a2206206994597c13d831ec7.factory.bridge.near'] =
+    specialTokenMetaMap['usdt.fakes.testnet'];
+
+// USDT Native
+specialTokenMetaMap['usdtt.fakes.testnet'] =
+    specialTokenMetaMap['usdt.tether-token.near'];
+
+// USDC Bridged
+specialTokenMetaMap['a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.factory.bridge.near'] =
+    specialTokenMetaMap['usdc.fakes.testnet'];
+
+export const formatToken = (token: Wallet.Token): Wallet.Token => {
     const { contractName } = token;
 
-    const isBridgedUSDT = contractName === 'usdt.fakes.testnet' || contractName === 'dac17f958d2ee523a2206206994597c13d831ec7.factory.bridge.near';
-    const isNativeUSDT = contractName === 'usdt.tether-token.near' || contractName === 'usdtt.fakes.testnet';
-    const isBridgedUSDC = contractName === 'usdc.fakes.testnet' || contractName === 'a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.factory.bridge.near';
-
-
-    if (isBridgedUSDT) {
-        return {
-            ...token,
-            onChainFTMetadata: {
-                ...token.onChainFTMetadata,
-                symbol: `${BRIDGED_CONSTANT} USDT`
-            }
-        };
-    }
-
-    if (isNativeUSDT) {
-        return {
-            ...token,
-            onChainFTMetadata: {
-                ...token.onChainFTMetadata,
-                symbol: 'Native USDT'
-            }
-        };
-    }
-
-    if (isBridgedUSDC) {
-        return {
-            ...token,
-            onChainFTMetadata: {
-                ...token.onChainFTMetadata,
-                symbol: `${BRIDGED_CONSTANT} USDC`
-            }
-        };
-    }
-
-    return token;
+    return {
+        ...token,
+        onChainFTMetadata: {
+            ...token.onChainFTMetadata,
+            ...specialTokenMetaMap[contractName],
+        },
+    };
 };
