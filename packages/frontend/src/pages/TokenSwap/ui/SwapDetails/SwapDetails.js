d6761fc5264609d49efe85314f2e803a124f1642
leehoemun <leehoemun@gmail.com>
2023-05-19 20:39:07 +0800

    fix: crash when swap input is too low

--- packages/frontend/src/pages/TokenSwap/ui/SwapDetails/SwapDetails.js
+++ packages/frontend/src/pages/TokenSwap/ui/SwapDetails/SwapDetails.js
@@ -5,10 +5,7 @@ import Accordion from '../../../../components/common/Accordion';
 import ChevronIcon from '../../../../components/svg/ChevronIcon';
 import { toSignificantDecimals } from '../../../../utils/amounts';
 import { useSwapData } from '../../model/Swap';
-import {
-    getMinAmountOut,
-    getSwapFeeAmount,
-} from '../../utils/calculations';
+import { getMinAmountOut, getSwapFeeAmount } from '../../utils/calculations';
 import { DECIMALS_TO_SAFE } from '../../utils/constants';
 import FtSwapDetails from './FtSwapDetails';
 import NearTransformationDetails from './NearTransformationDetails';
@@ -41,31 +38,33 @@ export default function SwapDetails() {
         amountOut,
         slippage,
     });
-    const minAmountOutToShow = toSignificantDecimals(
-        minAmountOut,
-        DECIMALS_TO_SAFE
-    );
+    const minAmountOutToShow = toSignificantDecimals(minAmountOut, DECIMALS_TO_SAFE);
 
     const isVisible = tokenIn && tokenOut && Number(amountIn);
-    const price =
-        isVisible && amountOut
-            ? toSignificantDecimals(
+    let price = '-';
+
+    if (isVisible && amountOut) {
+        if (Number(amountOut) !== 0) {
+            price = toSignificantDecimals(
                 Big(amountIn).div(amountOut).toFixed(),
                 DECIMALS_TO_SAFE
-            ) : '-';
+            );
+        }
+    }
 
     return (
         <SwapDetailsWrapper className={`${isVisible ? 'visible' : ''}`}>
-            <AccordionTitle id="swapDetailsTitle" className={`${isActive ? 'active' : ''}`} onClick={toggleDetailsView}>
+            <AccordionTitle
+                id="swapDetailsTitle"
+                className={`${isActive ? 'active' : ''}`}
+                onClick={toggleDetailsView}
+            >
                 1 {tokenOut?.onChainFTMetadata?.symbol}
                 {' = '}
                 {price} {tokenIn?.onChainFTMetadata?.symbol}
                 <ChevronIcon color="var(--mnw-color-1)" />
             </AccordionTitle>
-            <Accordion
-                trigger='swapDetailsTitle'
-                className='breakdown'
-            >
+            <Accordion trigger="swapDetailsTitle" className="breakdown">
                 {isNearTransformation ? (
                     <NearTransformationDetails
                         minAmountOut={amountIn}
