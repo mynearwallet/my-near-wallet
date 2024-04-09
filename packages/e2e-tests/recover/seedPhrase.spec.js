06fccd4a6081d4c6fd4d50a7fa5295e28241d9b2
Kelvin Hong <kelvinok52@gmail.com>
2024-04-09 15:56:14 +0700

    fix(account): allow import account zero balance (#230)

--- packages/e2e-tests/recover/seedPhrase.spec.js
+++ packages/e2e-tests/recover/seedPhrase.spec.js
@@ -3,6 +3,8 @@ const { test, expect } = require('@playwright/test');
 
 const { getEnvTestAccount } = require('../utils/account');
 const { createPassword } = require('../utils/password');
+const nearApiJsConnection = require('../utils/connectionSingleton');
+const { WALLET_NETWORK } = require('../constants');
 
 const { describe, beforeAll } = test;
 
@@ -36,6 +38,71 @@ describe('Account Recovery Using Seed Phrase', () => {
         await expect(page).toHaveURL(/\/$/);
         await expect(page.locator('data-test-id=currentUser >> visible=true')).toHaveText(
             testAccount.accountId
+        );
+    });
+
+    test('should show error when import deleted private key account', async ({
+        page,
+    }) => {
+        if (nearApiJsConnection.config?.networkId === WALLET_NETWORK.MAINNET) {
+            test.skip();
+        }
+        await page.goto('/recover-seed-phrase');
+
+        await createPassword(page);
+
+        // testprivatekey.testnet
+        await page.fill(
+            'data-test-id=seedPhraseRecoveryInput',
+            'first scheme behind humor senior fantasy fever wine ask vocal certain judge'
+        );
+        await page.click('data-test-id=seedPhraseRecoverySubmitButton');
+
+        await expect(page.locator('data-test-id=alertContainer')).toHaveText(
+            /The imported key does not match the public key/
+        );
+    });
+
+    test('should able import implicit zero balance for deleted account', async ({
+        page,
+    }) => {
+        if (nearApiJsConnection.config?.networkId === WALLET_NETWORK.MAINNET) {
+            test.skip();
+        }
+        await page.goto('/recover-seed-phrase');
+
+        await createPassword(page);
+
+        // testdelete.testnet
+        await page.fill(
+            'data-test-id=seedPhraseRecoveryInput',
+            'wreck brand tape bachelor network good glass scrap detail young mind cycle'
+        );
+        await page.click('data-test-id=seedPhraseRecoverySubmitButton');
+
+        await expect(page.locator('data-test-id=modalContainer')).toHaveText(
+            /The following account was successfully imported using the passphrase you provided:09be04986226a2cf13ed713f4e06864f38efe43af852b6cc4243d719aeaade8a/
+        );
+    });
+
+    test('should able import implicit zero balance for not funded new account', async ({
+        page,
+    }) => {
+        if (nearApiJsConnection.config?.networkId === WALLET_NETWORK.MAINNET) {
+            test.skip();
+        }
+        await page.goto('/recover-seed-phrase');
+
+        await createPassword(page);
+
+        await page.fill(
+            'data-test-id=seedPhraseRecoveryInput',
+            'false know tube produce tunnel sheriff radio kick ordinary rather alone kiss'
+        );
+        await page.click('data-test-id=seedPhraseRecoverySubmitButton');
+
+        await expect(page.locator('data-test-id=modalContainer')).toHaveText(
+            /The following account was successfully imported using the passphrase you provided:fd8abdb79b7e7fd13f48a705d1a3fb53390461339f718c365c13ef14008c1bfa/
         );
     });
 });
