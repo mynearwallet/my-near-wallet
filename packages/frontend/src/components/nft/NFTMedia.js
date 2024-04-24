587deb0bdca14d97fd159163fa17e85b3ed36e1c
superokid <kelvinok52@gmail.com>
2024-04-24 08:40:51 +0700

    fix(nft): video mimetype

--- packages/frontend/src/components/nft/NFTMedia.js
+++ packages/frontend/src/components/nft/NFTMedia.js
@@ -4,17 +4,20 @@ import FailedToLoad from '../../images/failed_to_load.svg';
 import ImageWithLoading from '../common/image/ImageWithLoading';
 
 export function NFTMedia({ mediaUrl, autoPlay = false, mimeType }) {
-    const [isVideo, mimeTypeExtension] = useMemo(() => {
-        let mimeTypeExtension;
+    const [isVideo, urlMimeType] = useMemo(() => {
+        let urlMimeType;
         // check mediaUrl string for .webm or .mp4 endings (case-insensitive)
         if (mediaUrl && mediaUrl.match(/\.webm$/i)) {
-            mimeTypeExtension = 'webm';
+            urlMimeType = 'webm';
         } else if (mediaUrl && mediaUrl.match(/\.mp4$/i)) {
-            mimeTypeExtension = 'mp4';
+            urlMimeType = 'mp4';
         }
+
+        let isMimeTypeVideo = mimeType && mimeType.match('video');
+
         // if there is a mediaUrl and a truthy mimeType (webm or mp4), we have a video
-        const isVideo = !!mimeType || (!!mediaUrl && mimeTypeExtension);
-        return [isVideo, mimeTypeExtension];
+        const isVideo = !!isMimeTypeVideo || (!!mediaUrl && urlMimeType);
+        return [isVideo, urlMimeType];
     }, [mediaUrl, mimeType]);
 
     if (isVideo) {
@@ -22,7 +25,7 @@ export function NFTMedia({ mediaUrl, autoPlay = false, mimeType }) {
             <video muted={true} loop controls autoPlay={autoPlay}>
                 <source
                     src={mediaUrl}
-                    type={mimeType || `video/${mimeTypeExtension}`}
+                    type={mimeType || `video/${urlMimeType}`}
                     onError={(e) => {
                         e.target.onerror = null;
                         e.target.parentElement.setAttribute('poster', FailedToLoad);
