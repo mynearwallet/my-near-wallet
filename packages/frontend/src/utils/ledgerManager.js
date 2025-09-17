6e0e7bb38ef8ba00f5454d957e93157288e8d228
superokid <kelvinok52@gmail.com>
2025-09-17 12:56:11 +0800

    fix: sign message start from ledger 2.3.5

--- packages/frontend/src/utils/ledgerManager.js
+++ packages/frontend/src/utils/ledgerManager.js
@@ -1,4 +1,4 @@
-import { createClient, getSupportedTransport, setDebugLogging } from 'near-ledger-js';
+import { getSupportedTransport, setDebugLogging } from 'near-ledger-js';
 
 setDebugLogging(false);
 
@@ -66,3 +66,72 @@ class LedgerManager {
 }
 
 export const ledgerManager = new LedgerManager();
+
+function bip32PathToBytes(path) {
+    const parts = path.split('/');
+    return Buffer.concat(
+        parts
+            .map((part) =>
+                part.endsWith("'")
+                    ? Math.abs(parseInt(part.slice(0, -1))) | 0x80000000
+                    : Math.abs(parseInt(part))
+            )
+            .map((i32) =>
+                Buffer.from([
+                    (i32 >> 24) & 0xff,
+                    (i32 >> 16) & 0xff,
+                    (i32 >> 8) & 0xff,
+                    i32 & 0xff,
+                ])
+            )
+    );
+}
+
+const networkId = 'W'.charCodeAt(0);
+const DEFAULT_PATH = "44'/397'/0'/0'/1'";
+async function createClient(transport) {
+    return {
+        transport,
+        async getVersion() {
+            const response = await this.transport.send(0x80, 6, 0, 0);
+            const [major, minor, patch] = Array.from(response);
+            return `${major}.${minor}.${patch}`;
+        },
+        async getPublicKey(path) {
+            path = path || DEFAULT_PATH;
+            const response = await this.transport.send(
+                0x80,
+                4,
+                0,
+                networkId,
+                bip32PathToBytes(path)
+            );
+            return Buffer.from(response.subarray(0, -2));
+        },
+        async sign(transactionData, path) {
+            // NOTE: getVersion call allows to reset state to avoid starting from partially filled buffer
+            const version = await this.getVersion();
+            console.info('Ledger app version:', version);
+
+            path = path || DEFAULT_PATH;
+            transactionData = Buffer.from(transactionData);
+            // 128 - 5 service bytes
+            const CHUNK_SIZE = 123;
+            const allData = Buffer.concat([bip32PathToBytes(path), transactionData]);
+            for (let offset = 0; offset < allData.length; offset += CHUNK_SIZE) {
+                const chunk = Buffer.from(allData.subarray(offset, offset + CHUNK_SIZE));
+                const isLastChunk = offset + CHUNK_SIZE >= allData.length;
+                const response = await this.transport.send(
+                    0x80,
+                    7,
+                    isLastChunk ? 0x80 : 0,
+                    networkId,
+                    chunk
+                );
+                if (isLastChunk) {
+                    return Buffer.from(response.subarray(0, -2));
+                }
+            }
+        },
+    };
+}
--- packages/frontend/src/utils/signMessage.ts
+++ packages/frontend/src/utils/signMessage.ts
@@ -1,17 +1,15 @@
-import * as borsh from 'borsh';
+import { BorshSchema, borshSerialize } from 'borsher';
 
 //
 // https://github.com/near/NEPs/blob/master/neps/nep-0413.md
 //
-const schema = {
-    struct: {
-        tag: 'u32',
-        message: 'string',
-        nonce: { array: { type: 'u8', len: 32 } },
-        recipient: 'string',
-        callbackUrl: { option: 'string' },
-    },
-};
+
+export const messageNep413BorshSchema = BorshSchema.Struct({
+    message: BorshSchema.String,
+    nonce: BorshSchema.Array(BorshSchema.u8, 32),
+    recipient: BorshSchema.String,
+    callbackUrl: BorshSchema.Option(BorshSchema.String),
+});
 
 const isBase64 = (value: string) =>
     /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$/.test(
@@ -24,9 +22,6 @@ export const validateNonce = (nonce?: string) => {
     }
     return nonce && nonce.length === 32;
 };
-
-export const serialize = (value) => borsh.serialize(schema, value);
-export const deserialize = (value) => borsh.deserialize(schema, value);
 
 export const messageToSign = (data: {
     message: string;
@@ -43,5 +38,5 @@ export const messageToSign = (data: {
         nonce,
     };
 
-    return serialize(payload);
+    return borshSerialize(messageNep413BorshSchema, payload);
 };
