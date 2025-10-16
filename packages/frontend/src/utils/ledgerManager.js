9aea51b1edca2747241ea00d7f33aa61d2fa1866
Kelvin Hong <kelvinok52@gmail.com>
2025-10-16 13:30:19 +0800

    fix ledger signmessage (#310)
    * fix: sign message start from ledger 2.3.5
    
    * fix: throw ledger error message
    
    * fix: version update ledger nearapp
    
    * fix: revert library borsher
    
    * fix(ledger): sign message should works both nep and regular sign
    
    * fix: import account only for available indexer

--- packages/frontend/src/utils/ledgerManager.js
+++ packages/frontend/src/utils/ledgerManager.js
@@ -109,7 +109,7 @@ async function createClient(transport) {
             );
             return Buffer.from(response.subarray(0, -2));
         },
-        async signMessageNep413(transactionData, path) {
+        async signMessage(transactionData, path, isNep413) {
             // NOTE: getVersion call allows to reset state to avoid starting from partially filled buffer
             const response = await this.transport.send(0x80, 6, 0, 0);
             const [major, minor] = Array.from(response);
@@ -123,13 +123,15 @@ async function createClient(transport) {
             transactionData = Buffer.from(transactionData);
             // 128 - 5 service bytes
             const CHUNK_SIZE = 123;
+            const INSTRUCTION_SIGNMESSAGE = 2;
+            const INSTRUCTION_SIGNMESSAGE_NEP413 = 7;
             const allData = Buffer.concat([bip32PathToBytes(path), transactionData]);
             for (let offset = 0; offset < allData.length; offset += CHUNK_SIZE) {
                 const chunk = Buffer.from(allData.subarray(offset, offset + CHUNK_SIZE));
                 const isLastChunk = offset + CHUNK_SIZE >= allData.length;
                 const response = await this.transport.send(
                     0x80,
-                    7,
+                    isNep413 ? INSTRUCTION_SIGNMESSAGE_NEP413 : INSTRUCTION_SIGNMESSAGE,
                     isLastChunk ? 0x80 : 0,
                     networkId,
                     chunk
