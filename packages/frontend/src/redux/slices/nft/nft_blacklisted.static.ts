a17236d9deba68fcfb0ee884cc187ad05e2866e8
superokid <kelvinok52@gmail.com>
2025-11-05 17:40:30 +0800

    feat: blacklisted nfts

---
+++ packages/frontend/src/redux/slices/nft/nft_blacklisted.static.ts
@@ -0,0 +1,65 @@
+export const nftBlacklisted = [
+    {
+        Collection: 'airdrop-ticket.near',
+    },
+    {
+        Collection: 'burrowfinancereward.mintbase1.near',
+    },
+    {
+        Collection: 'claim-rewards.near',
+    },
+    {
+        Collection: 'event-drop.near',
+    },
+    {
+        Collection: 'event-drops.near',
+    },
+    {
+        Collection: 'near-protocols.near',
+    },
+    {
+        Collection: 'near-rewards.near',
+    },
+    {
+        Collection: 'neardaorewards.mintbase1.near',
+    },
+    {
+        Collection: 'neargives.near',
+    },
+    {
+        Collection: 'nearprotocolreward.mintbase1.near',
+    },
+    {
+        Collection: 'nearprotolcol.near',
+    },
+    {
+        Collection: 'nearreward.mintbase1.near',
+    },
+    {
+        Collection: 'nearrewardvoucher2.mintbase1.near',
+    },
+    {
+        Collection: 'nearvoucherstore.mintbase1.near',
+    },
+    {
+        Collection: 'nft-reward.near',
+    },
+    {
+        Collection: 'nft-rewards.near',
+    },
+    {
+        Collection: 'nftsairdrop.near',
+    },
+    {
+        Collection: 'official-near.near',
+    },
+    {
+        Collection: 'stake-boost.near',
+    },
+    {
+        Token: ['x.paras.near', '508396:43'],
+    },
+    {
+        Token: ['x.paras.near', '508396:44'],
+    },
+];
